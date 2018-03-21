12718f85fb8561ee2a750171c2b8e5bac7109fc9
Ganang Sadewo <ganang59@gmail.com>
2018-03-22 04:24:38 +0700

    Tambah Penilaian SV

---
+++ app/src/main/java/com/example/app/atworkcsm/ConfirmPekerjaan.java
@@ -0,0 +1,13 @@
+package com.example.app.atworkcsm;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ConfirmPekerjaan extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_confirm_pekerjaan);
+    }
+}
